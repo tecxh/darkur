7cfe56e4a4d70b59add19e836a19630838b4f294
tecxh <techno.zelda@gmail.com>
2025-08-12 17:49:20 -0400

    add phosphor icons

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { HeartIcon } from '@phosphor-icons/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <p className="text-xl text-black">hello</p>
+    <HeartIcon />
   </StrictMode>,
 )
