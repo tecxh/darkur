f2c6cb688c7d83a448bb30abfa299f4f1472b3d2
tecxh <techno.zelda@gmail.com>
2025-08-12 18:07:52 -0400

    add provider as decorator for storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react-vite'
+import { DarkurProvider } from '../src/DarkurProvider/DarkurProvider';
 
 const preview: Preview = {
   parameters: {
@@ -8,7 +9,13 @@ const preview: Preview = {
        date: /Date$/i,
       },
     },
-
+    decorators: [
+      (Story) => (
+        <DarkurProvider>
+          <Story />
+        </DarkurProvider>
+      )
+    ],
     a11y: {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
